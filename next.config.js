a77bc9135c82303620b323f8ca302fc924be0925
jalenluorion <jalenlu2000@icloud.com>
2025-08-29 23:29:01 -0400

    istg

--- next.config.js
+++ next.config.js
@@ -9,6 +9,19 @@ const nextConfig = {
             },
         ],
     },
+    async headers() {
+        return [
+            {
+                source: '/:path*{/}?',
+                headers: [
+                    {
+                        key: 'X-Accel-Buffering',
+                        value: 'no',
+                    },
+                ],
+            },
+        ];
+    },
 };
 
 module.exports = nextConfig;
