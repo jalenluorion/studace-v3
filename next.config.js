6fcf0d8745c7f2cf19c483c7897102a02bddb6a7
jalenluorion <jalenlu2000@icloud.com>
2025-10-14 01:37:32 -0600

    is everything back to normal

--- next.config.js
+++ next.config.js
@@ -9,19 +9,6 @@ const nextConfig = {
             },
         ],
     },
-    async headers() {
-        return [
-            {
-                source: '/:path*{/}?',
-                headers: [
-                    {
-                        key: 'X-Accel-Buffering',
-                        value: 'no',
-                    },
-                ],
-            },
-        ];
-    },
 };
 
 module.exports = nextConfig;
